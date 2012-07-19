a018c4d6f757c73ca7599f140b34e25700c080f1
Aidan Delaney <aidan@phoric.eu>
2012-07-19 20:16:36 +0100

    Purged DEB from decomposition/*

--- devel/iCircles/src/icircles/decomposition/Decomposer.java
+++ devel/iCircles/src/icircles/decomposition/Decomposer.java
@@ -1,17 +1,51 @@
 package icircles.decomposition;
 
+/*
+ * @author Jean Flower <jeanflower@rocketmail.com>
+ * Copyright (c) 2012
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of the iCircles Project.
+ */
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
-import icircles.util.DEB;
-
 import icircles.abstractDescription.AbstractDescription;
 import icircles.abstractDescription.AbstractCurve;
 import icircles.abstractDescription.AbstractBasicRegion;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.Level;
+
 public class Decomposer {
+
+    static Logger logger = Logger.getLogger(Decomposer.class.getName());
 
     DecompositionStrategy s;
     ArrayList<AbstractCurve> toRemove = new ArrayList<AbstractCurve>(); // some utility data
@@ -73,13 +107,13 @@ public class Decomposer {
                 ad = step.target();
             }
         }
-        if (DEB.level >= 1) {
-            System.out.println("decomposition begin : ");
-            for (DecompositionStep step : result) {
-                System.out.println("step : " + step.debug());
-            }
-            System.out.println("decomposition end ");
+
+        logger.debug("decomposition begin : ");
+        for (DecompositionStep step : result) {
+                logger.debug("step : " + step.debug());
         }
+        logger.debug("decomposition end ");
+
         return result;
     }
     /*
--- devel/iCircles/src/icircles/decomposition/DecompositionStep.java
+++ devel/iCircles/src/icircles/decomposition/DecompositionStep.java
@@ -1,16 +1,50 @@
 package icircles.decomposition;
+/*
+ * @author Jean Flower <jeanflower@rocketmail.com>
+ * Copyright (c) 2012
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of the iCircles Project.
+ */
 
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
-import icircles.util.DEB;
 import icircles.abstractDescription.AbstractDescription;
 import icircles.abstractDescription.AbstractCurve;
 import icircles.abstractDescription.AbstractBasicRegion;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.Level;
+
 public class DecompositionStep {
+
+    static Logger logger = Logger.getLogger(DecompositionStep.class.getName());
 
     AbstractDescription m_from;
     AbstractDescription m_to;
@@ -34,25 +68,24 @@ public class DecompositionStep {
     }
 
     public String debug() {
-        if (DEB.level == 0) {
+        if (logger.getEffectiveLevel().equals(Level.ALL)) {
             return "";
         }
+        // otherwise we want output
+
         StringBuilder sb = new StringBuilder();
         sb.append("remove ");
         sb.append(m_from.printContour(m_removed));
-        if (DEB.level > 1) {
-            sb.append("\n");
-        }
+        sb.append("\n");
+
         sb.append(" from ");
         sb.append(m_from.debugAsSentence());
-        if (DEB.level > 1) {
-            sb.append("\n");
-        }
+        sb.append("\n");
+
         sb.append(" to ");
         sb.append(m_to.debugAsSentence());
-        if (DEB.level > 1) {
-            sb.append("\n");
-        }
+        sb.append("\n");
+
         sb.append(" zonesMoved: ");
         Set<Map.Entry<AbstractBasicRegion, AbstractBasicRegion>> entries = m_zones_moved.entrySet();
         for (Map.Entry<AbstractBasicRegion, AbstractBasicRegion> z_map : entries) {
